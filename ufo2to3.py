f266f6ecd9595a5032160bead4b03a3e9689721c
Chris Simpkins <git.simpkins@gmail.com>
2018-09-20 11:57:34 -0400

    [ufo2to3.py] black formatting

--- ufo2to3.py
+++ ufo2to3.py
@@ -11,15 +11,21 @@ import sys
 
 import defcon
 
-def main(argv):
-	for ufo in argv:
-		try:
-			font = defcon.Font(ufo)
-			font.save(formatVersion=3)
-			print("Successfully converted " + ufo)
-		except Exception as e:
-			sys.stderr.write("[ERROR] There was an error with the conversion attempt: " + os.linesep + str(e))
-			sys.exit(1)
 
-if __name__ == '__main__':
-	main(sys.argv[1:])
+def main(argv):
+    for ufo in argv:
+        try:
+            font = defcon.Font(ufo)
+            font.save(formatVersion=3)
+            print("Successfully converted " + ufo)
+        except Exception as e:
+            sys.stderr.write(
+                "[ERROR] There was an error with the conversion attempt: "
+                + os.linesep
+                + str(e)
+            )
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
